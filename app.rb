72bdc2f84a677d728443ffa86bc5b24b574a694e
Jack Polgar <jack@polgar.id.au>
2011-06-13 09:23:55 +1000

    Tweak

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ require __DIR__('config/routes')
 # Make sure that Ramaze knows where you are
 Ramaze.options.roots = [__DIR__]
 
-class Bignum
+class Object
   def ago_in_words
     Time.at(self).ago_in_words.gsub(' ago', '')
   end
